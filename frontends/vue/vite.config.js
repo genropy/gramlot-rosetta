8b8ee11aeed8237fbdc9fefc0a5b1bd4fdb03fa5
Giovanni Porcari <giovanni.porcari@gmail.com>
2026-09-08 10:36:29 +0200

    Reduce React and Vue examples to hello world

--- frontends/vue/vite.config.js
+++ frontends/vue/vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/vue/',
+  base: '/examples/vue/hello-world/',
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': 'http://127.0.0.1:8026',
       '/shared': 'http://127.0.0.1:8026',
     },
   },
--- standby/orders/frontends/react/vite.config.js
+++ standby/orders/frontends/react/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/examples/react/hello-world/',
+  base: '/react/',
   plugins: [react()],
   server: {
     proxy: {
+      '/api': 'http://127.0.0.1:8026',
       '/shared': 'http://127.0.0.1:8026',
     },
   },
