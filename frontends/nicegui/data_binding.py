7abfbda48130d6f2c75adc8cd325350eff1e269d
Giovanni Porcari <giovanni.porcari@gmail.com>
2026-09-11 20:07:21 +0200

    Initialize live input before binding to prevent delayed first-edit reset [skip ci]

--- frontends/nicegui/data_binding.py
+++ frontends/nicegui/data_binding.py
@@ -6,7 +6,7 @@ from nicegui import ui
 def data_binding():
     data = {'live_message': 'Hello World', 'message': 'Hello World'}
     ui.html(tag='h1').bind_content_from(data, 'live_message', backward=escape)
-    ui.input('Live').bind_value(data, 'live_message')
+    ui.input('Live', value=data['live_message']).bind_value(data, 'live_message')
     ui.html(tag='h1').bind_content_from(data, 'message', backward=escape)
     ui.input('On focus out', value=data['message']).on(
         'blur', lambda event: data.update(message=event.sender.value))
